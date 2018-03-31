e3bb5b9f4282447415bb4a0b7119e2442322eae6
xiaopongcheng <820961283@qq.com>
2018-03-31 16:21:45 +0800

    commit ptoject

---
+++ mail163/test_case/models/myunit.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from .driver import broswer
+import unittest
+import os
+
+class MyTest(unittest.TestCase):
+    def setUp(self):
+        self.driver = broswer()
+        self.driver.implicitly_wait(10)
+        self.driver.maximize_window()
+
+    def tearDown(self):
+        pass
+        self.driver.quit()
